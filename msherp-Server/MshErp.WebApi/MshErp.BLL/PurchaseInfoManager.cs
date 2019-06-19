ea0db548a9276ab80434ca903f97c03414252219
lee <lee@12.com>
2019-06-19 10:09:59 +0800

    add files

---
+++ msherp-Server/MshErp.WebApi/MshErp.BLL/PurchaseInfoManager.cs
@@ -0,0 +1,14 @@
+﻿using MshErp.BLL.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MshErp.BLL
+{
+    public class PurchaseInfoManager: IPurchaseInfoManager
+    {
+
+    }
+}
